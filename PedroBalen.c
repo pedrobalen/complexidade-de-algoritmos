044e00c1ae81911bd7dc12f68e67827f8f9fb173
Pedro Balen <p.balen99@gmail.com>
2025-08-20 08:59:11 -0300

    Add files via upload

---
+++ PedroBalen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;               // atribuição
+    int b = 3;               // atribuição
+    int soma = a + b;        // atribuição
+
+    if (soma > 7) {          // comparação
+        printf("Maior que 7\n");  // operação
+    }
+
+    printf("%d\n", soma);    // atribuição
+
+    return 0;                
+}
